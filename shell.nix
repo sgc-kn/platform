39c7cda5dfcaf6807fc029e94e6b18356f90ef27
Patrik Keller <git@pkel.dev>
2024-09-02 10:44:13 +0200

    shell.nix: install k8s tools

--- shell.nix
+++ shell.nix
@@ -5,7 +5,15 @@ pkgs.mkShell {
     pkgs.age
     pkgs.sops
 
+    pkgs.minikube
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+
     # keep this line if you use bash
     pkgs.bashInteractive
   ];
+
+  shellHook = ''
+    alias kubectl="minikube kubectl --"
+  '';
 }
