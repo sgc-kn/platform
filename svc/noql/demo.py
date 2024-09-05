63654cb97d999ea5d0e3e58b20d1c1092a9d6d3c
Patrik Keller <git@pkel.dev>
2024-09-05 14:47:30 +0200

    draft quantumleap replacement

---
+++ svc/noql/demo.py
@@ -0,0 +1,98 @@
+import httpx
+import json
+import sys
+import time
+
+# helper function; exit on http error
+
+def req(*args, **kwargs):
+    try:
+        r = httpx.request(*args, **kwargs)
+        r.raise_for_status()
+    except httpx.HTTPError as e:
+        print(e)
+        print(r.text)
+        sys.exit(1)
+
+    return r
+
+# prepare table
+
+stmts = [
+        'DROP TABLE IF EXISTS table0',
+        '''
+        CREATE TABLE table0 (
+          time timestamptz,
+          value double precision
+        );
+        ''',
+        "SELECT create_hypertable('table0', 'time')",
+        ]
+
+for stmt in stmts:
+    req('POST',
+        'http://localhost:8000/api/rpc/mgmt',
+        headers = {'Content-Type': 'text/plain'},
+        content = stmt)
+
+time.sleep(1)
+
+# post data as csv
+
+data = '''time,value
+2023-01-01 00:00:00+00,100.0
+2023-01-01 01:00:00+00,101.5
+2023-01-01 02:00:00+00,102.2
+2023-01-01 03:00:00+00,103.9
+2023-01-01 04:00:00+00,105.1
+2023-01-01 05:00:00+00,106.7
+2023-01-01 06:00:00+00,108.0
+2023-01-01 07:00:00+00,109.4
+2023-01-01 08:00:00+00,110.8
+2023-01-01 09:00:00+00,112.3
+2023-01-01 10:00:00+00,113.7
+2023-01-01 11:00:00+00,115.1
+2023-01-01 12:00:00+00,116.5
+2023-01-01 13:00:00+00,118.0
+2023-01-01 14:00:00+00,119.4
+2023-01-01 15:00:00+00,120.7
+2023-01-01 16:00:00+00,122.1
+2023-01-01 17:00:00+00,123.4
+2023-01-01 18:00:00+00,124.8
+2023-01-01 19:00:00+00,126.2
+'''
+
+req('POST',
+    'http://localhost:8000/api/table0',
+    content = data,
+    headers = { 'Content-Type': 'text/csv' },
+    )
+
+# post data as json
+
+data = [
+        dict( time = '2023-01-01 20:00:00+00', value = 101.8),
+        dict( time = '2023-01-01 21:00:00+00', value = 104.7),
+        ]
+
+req('POST',
+    'http://localhost:8000/api/table0',
+    json = data,
+    )
+
+# read some values from the end of the table as json
+
+r = req('GET',
+        'http://localhost:8000/api/table0',
+        params=dict(time = 'gte.2023-01-01 17:00:00+00')
+        )
+print(json.dumps(r.json(), indent=2))
+
+# read some values from the top of the table as csv
+
+r = req('GET',
+        'http://localhost:8000/api/table0',
+        params=dict(time = 'lte.2023-01-01 04:00:00+00'),
+        headers=dict(Accept = 'text/csv'),
+        )
+print(r.text)
---
+++ svc/noql/setup.sql
@@ -0,0 +1,30 @@
+--
+-- Extension Setup
+--
+
+-- CREATE SCHEMA extensions;
+-- CREATE EXTENSION timescaledb SCHEMA extensions;
+-- the timescale docker image does this for us!
+
+-- CREATE EXTENSION postgis SCHEMA extensions;
+-- not available on timescale base docker image
+
+--
+-- Setup schema for the API
+--
+
+CREATE SCHEMA postgrest;
+SET search_path = postgrest, public;
+
+--
+-- Management Interface
+-- execute arbitrary SQL from HTTP
+--
+
+CREATE OR REPLACE FUNCTION mgmt( TEXT ) RETURNS void AS
+$$
+BEGIN
+  EXECUTE $1;
+  NOTIFY pgrst, 'reload schema';
+END;
+$$ LANGUAGE plpgsql;
