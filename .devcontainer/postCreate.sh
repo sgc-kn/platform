c68fcf0d964b36ea39b0f542496d973c5429613c
Patrik Keller <git@pkel.dev>
2025-02-03 14:28:46 +0000

    install just

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -10,8 +10,8 @@ git config --global diff.lfs.textconv cat # diff large files like usual files
 git submodule update --init --recursive
 git submodule foreach --recursive git lfs pull # first git lfs pull for the submodules, automated by git lfs install from now on
 
-# install uv (used for Python package management)
-pipx install uv
+# install uv (used for Python package management) and just (for small maintenance scripts)
+pipx install rust-just uv
 
 # create Python virtual environment .venv with all Python dependencies (used as default Python interpreter)
 uv sync
