e4e39eff0a681b6d9268d0c5ecc12ec0d051987f
Patrik Keller <git@pkel.dev>
2025-03-06 17:41:51 +0100

    dagster notebook runner

--- integrations/sources/dagster_ipynb_test/dagster.py
+++ integrations/sources/dagster_ipynb_test/dagster.py
@@ -1,22 +1,55 @@
 import dagster
+import os
+import shutil
 import subprocess
+import tempfile
 
-@dagster.asset
-def notebook() -> None:
-    subprocess.run(['papermill',
-                    dagster.file_relative_path(__file__, "notebook.ipynb"),
-                    dagster.file_relative_path(__file__, "notebook-out.ipynb"),
-                    '--log-output',
-                    ], check=True)
+def evaluate_notebook(infile, outfile):
+    # evaluate notebook infile (path), store result in outfile (path)
+    # return true if the notebook evaluated alright, false otherwise
+    try:
+        subprocess.run(['papermill',
+                        infile,
+                        outfile,
+                        '--log-output',
+                        ], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def convert_notebook(ipynb):
+    # render notebook ipynb (path) into html file, changing the extension from
+    # ipynb to html. Raise on error.
     subprocess.run(['jupyter',
                     'nbconvert',
-                    dagster.file_relative_path(__file__, "notebook-out.ipynb"),
                     '--to', 'html',
+                    ipynb
                     ], check=True)
 
+def evaluate_and_convert_notebook(src, dst):
+    basename = os.path.basename(src)
+    htmlname = os.path.splitext(basename)[0] + '.html'
+    with tempfile.TemporaryDirectory() as tmp:
+        tmpipynb = f'{tmp}/{basename}'
+        tmphtml = f'{tmp}/{htmlname}'
+        success = evaluate_notebook(src, tmpipynb)
+
+        convert_notebook(tmpipynb)
+        shutil.move(tmphtml, dst)
+
+    if not success:
+        raise RuntimeError("notebook evaluation failed. Find partially rendered notebook at: " + dst)
+
+@dagster.asset
+def notebook():
+    name = "notebook"
+    src = dagster.file_relative_path(__file__, name + ".ipynb")
+    dst = dagster.file_relative_path(__file__, name + ".html")
+    evaluate_and_convert_notebook(src, dst)
+    output = dagster.MetadataValue.url('file://' + dst)
     return dagster.MaterializeResult(
         metadata={
-            "output": dagster.file_relative_path(__file__, "notebook-out.html")
+            "output": output,
         }
     )
 
@@ -27,7 +60,7 @@ job = dagster.define_asset_job(
 schedule = dagster.ScheduleDefinition(
         job=job,
         cron_schedule="*/5 * * * *",
-        default_status=dagster.DefaultScheduleStatus.RUNNING,
+        #  default_status=dagster.DefaultScheduleStatus.RUNNING,
         )
 
 from utils.dagster import registry as dagster_registry
