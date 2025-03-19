cd2482b943e6c857f4e2a6e7945914a68183cec9
Patrik Keller <git@pkel.dev>
2025-03-19 10:49:52 +0100

    hide notebook execution logic from job definition

--- integrations/sources/sgc-weather/jobs.py
+++ integrations/sources/sgc-weather/jobs.py
@@ -1,52 +1,8 @@
 # platform: load=true
 
-import dagster
-import os
-import shutil
-import subprocess
-import tempfile
-from utils.jobs import job
+from utils.jobs import job, run_notebook
 
-
-def evaluate_notebook(infile, outfile):
-    # evaluate notebook infile (path), store result in outfile (path)
-    # return true if the notebook evaluated alright, false otherwise
-    try:
-        subprocess.run(['papermill',
-                        infile,
-                        outfile,
-                        '--log-output',
-                        ], check=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
-
-def convert_notebook(ipynb):
-    # render notebook ipynb (path) into html file, changing the extension from
-    # ipynb to html. Raise on error.
-    subprocess.run(['jupyter',
-                    'nbconvert',
-                    '--to', 'html',
-                    ipynb
-                    ], check=True)
-
-def evaluate_and_convert_notebook(src, dst):
-    basename = os.path.basename(src)
-    htmlname = os.path.splitext(basename)[0] + '.html'
-    with tempfile.TemporaryDirectory() as tmp:
-        tmpipynb = f'{tmp}/{basename}'
-        tmphtml = f'{tmp}/{htmlname}'
-        success = evaluate_notebook(src, tmpipynb)
-
-        convert_notebook(tmpipynb)
-        shutil.move(tmphtml, dst)
-
-    if not success:
-        raise RuntimeError("notebook evaluation failed. Find partially rendered notebook at: " + dst)
 
 @job(group='sgc_weather', cron_schedule="27 * * * *")
 def raw_tti_messages_v0():
-    name = "sync-raw-tti-messages-v0"
-    src = dagster.file_relative_path(__file__, name + ".ipynb")
-    dst = dagster.file_relative_path(__file__, name + ".html")
-    evaluate_and_convert_notebook(src, dst)
+    run_notebook("sync-raw-tti-messages-v0", relative_to=__file__)
--- utils/jobs/__init__.py
+++ utils/jobs/__init__.py
@@ -1,6 +1,10 @@
 from typing import Callable, Optional
 from utils.dagster import registry as dagster_registry
 import dagster
+import os
+import shutil
+import subprocess
+import tempfile
 
 # Thin wrapper around dagster assets, jobs, and schedules to enable
 # short definitions like the following.
@@ -56,3 +60,51 @@ def job(*args, **kwargs):
         dagster_registry.register(defs)
         return None # intentionally no `defs` to hide dagster internals
     return decorator
+
+## Additional wrapper schedule the evaluation of notebooks
+
+def _evaluate_notebook(infile, outfile):
+    # evaluate notebook infile (path), store result in outfile (path)
+    # return true if the notebook evaluated alright, false otherwise
+    try:
+        subprocess.run(['papermill',
+                        infile,
+                        outfile,
+                        '--log-output',
+                        ], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def _convert_notebook(ipynb):
+    # render notebook ipynb (path) into html file, changing the extension from
+    # ipynb to html. Raise on error.
+    subprocess.run(['jupyter',
+                    'nbconvert',
+                    '--to', 'html',
+                    ipynb
+                    ], check=True)
+
+def _evaluate_and_convert_notebook(src, dst):
+    basename = os.path.basename(src)
+    htmlname = os.path.splitext(basename)[0] + '.html'
+    with tempfile.TemporaryDirectory() as tmp:
+        tmpipynb = f'{tmp}/{basename}'
+        tmphtml = f'{tmp}/{htmlname}'
+        success = _evaluate_notebook(src, tmpipynb)
+
+        _convert_notebook(tmpipynb)
+        shutil.move(tmphtml, dst) # TODO persist this on S3
+
+    if not success:
+        raise RuntimeError("notebook evaluation failed. Find partially rendered notebook at: " + dst)
+
+def run_notebook(notebook: str, *, relative_to: str):
+    name = notebook.removesuffix('.ipynb')
+    src = dagster.file_relative_path(relative_to, name + ".ipynb")
+    dst = dagster.file_relative_path(relative_to, name + ".html")
+    _evaluate_and_convert_notebook(src, dst)
+
+
+
+
