e129b631420ff3ebf2c59a1f5658b49961e9af1e
Patrik Keller <git@pkel.dev>
2025-03-19 13:10:56 +0100

    sgc-weather. adjust schema
    store json messages as string

--- integrations/sources/sgc-weather/jobs.py
+++ integrations/sources/sgc-weather/jobs.py
@@ -4,5 +4,5 @@ from utils.jobs import job, run_notebook
 
 
 @job(group='sgc_weather', cron_schedule="27 * * * *")
-def raw_tti_messages_v0():
-    run_notebook("sync-raw-tti-messages-v0", relative_to=__file__)
+def raw_tti_messages():
+    run_notebook("sync-raw-tti-messages", relative_to=__file__)
