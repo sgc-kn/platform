efe0aabd755b1a75da4beb39287b1224a5ec1763
Patrik Keller <git@pkel.dev>
2025-03-31 14:49:41 +0200

    sgc-weather. table maintenance job

--- integrations/sources/sgc-weather/jobs.py
+++ integrations/sources/sgc-weather/jobs.py
@@ -3,8 +3,14 @@
 from utils.jobs import job, run_notebook
 
 
-@job(group='sgc_weather', cron_schedule="27 * * * *")
+@job(group='sgc_weather', cron_schedule="27 * * * *") # hourly, xx:27
 def raw_tti_messages(context):
     run_notebook("sync-raw-tti-messages",
+                 relative_to=__file__,
+                 context=context)
+
+@job(group='sgc_weather', cron_schedule="0 0 * * *") # daily, 00:00
+def maintain_tables(context):
+    run_notebook("maintain-tables",
                  relative_to=__file__,
                  context=context)
