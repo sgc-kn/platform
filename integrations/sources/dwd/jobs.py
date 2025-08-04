0365eee227bdd938c3bb37ca96b3eeb9c5250891
WBaumgartner <113365794+WBaumgartner@users.noreply.github.com>
2025-08-04 10:13:22 +0000

    Dagster_Test

--- integrations/sources/dwd/jobs.py
+++ integrations/sources/dwd/jobs.py
@@ -4,7 +4,7 @@ from utils.jobs import job, run_notebook
 
 
 @job(group='dwd', cron_schedule="*/5 * * * *") # every 5 minutes
-def latest_measurements(context):
+def get_warnings(context):
     run_notebook("warnmeldungen",
                  relative_to=__file__,
                  context=context)
